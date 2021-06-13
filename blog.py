7a6664721d551943c53bf0179da618f88812157d
YzyrG <zyr724725@gmail.com>
2021-06-13 18:43:45 +0800

    last commit & display posts & add new post

--- blog.py
+++ blog.py
@@ -2,11 +2,16 @@
 
 from flask import Flask, render_template, request, session, flash,\
 	redirect, url_for, g
-
+from functools import wraps
 import sqlite3
 
 # configuration
-DATABASE = "blog.db"
+DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = '\x1c}\x04\xb4\x15\x00\xc7\x9f\x86\xc9\x0bW\xe2\x07\x83\x91\xfc5\xafe\xf4Ax\xba'
+
+
 
 app = Flask(__name__)
 
@@ -18,15 +23,62 @@ def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
 #view:login.html
-@app.route('/')
-def login() :
-	return render_template('login.html')
+@app.route('/', methods=['GET', 'POST'])
+def login():
+	error = None
+	status_code = 200
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME'] or \
+				request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid Credentials. Please try again.'
+			status_code = 401
+		else:
+			session['logged_in'] = True
+			return redirect(url_for('main'))
+	return render_template('login.html', error=error), status_code
+
+# logout
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('login'))
+
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return test(*args, **kwargs)
+		else:
+			flash('You need to log in first.')
+			return redirect(url_for('login'))
+	return wrap
 
 # view:main.html
-@app.route('/mian')
+@app.route('/main')
+@login_required
 def main():
-	return render_template('main.html')
-	
+	g.db = connect_db()
+	cur = g.db.execute('select * from posts')
+	posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]	
+	return render_template('main.html', posts=posts)
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+	title = request.form['title']
+	post = request.form['post']
+	if not title or not post:
+		flash("All fields are required, Please try again.")
+		return redirect(url_for('main'))
+	else:
+		g.db = connect_db()
+		g.db.execute('insert into posts (title, post) values(?, ?)',
+			[request.form['title'], request.form['post']])
+		g.db.commit()
+		g.db.close()
+		flash('New entry was successfully posted!')
+		return redirect(url_for('main'))
 
 if __name__ == '__main__':
 	app.run(debug=True) 
